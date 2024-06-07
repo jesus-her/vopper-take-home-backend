fed10b5776f26122d017082cfae192030cac18ce
Jesús Hernández <jesus10.hn@gmail.com>
2024-06-07 14:45:38 -0600

    Add form and database validations for trainers

--- src/schema/trainer.schema.ts
+++ src/schema/trainer.schema.ts
@@ -4,16 +4,19 @@ const payload = {
   body: object({
     name: string({
       required_error: 'Name is required'
-    }),
+    }).min(1, 'Name should be at least 1 character long'),
+
     lastName: string({
       required_error: 'Last name is required'
     }).min(5, 'Last name should be at least 5 characters long'),
+
     medals: number({
       required_error: 'Medals is required'
-    }),
+    }).nonnegative('Medals should be a non-negative number'),
+
     phone: string({
       required_error: 'Phone is required'
-    })
+    }).regex(/^\d{10}$/, 'Phone should be a 10-digit number')
   })
 }
 
